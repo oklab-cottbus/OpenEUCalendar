09479e8c0812782fcfd845d465577eedc3b10a31
Sebastian Scholz <bastiman1@gmail.com>
2020-01-25 18:03:22 +0100

    funktion zur umformatierung der alten csv

--- read_data.R
+++ read_data.R
@@ -1,12 +1,22 @@
-root_path <- "/Users/Sebastian/Google_Drive/Dokumente/Programmieren/JupyterNotebook/ScrapeEUCommissionCalendar/"
-Sys.setlocale("LC_TIME", "de")
+root_path <- ""
+Sys.setlocale("LC_TIME", "en_US.UTF-8")
 
 get_calendar_csv <- function(){
   cal <- read.csv(paste(root_path,"Kalender.csv",sep=""),sep = ";")
-  Jahr <- 2020
-  Monat <- cal$Monat
-  Tag <- cal$TagZahl
-  cal$Datum <- as.POSIXct(strptime(paste(Jahr,Monat,Tag,sep="-"),"%Y-%b-%e"))
+  
   
   return(cal)
+}
+
+reformat_csv <- function(){
+  Jahr <- 2020
+  cal <- read.csv(paste(root_path,"Kalender.csv",sep=""),sep = ";")
+  cal$TimeAppoint <- as.POSIXlt((strptime(paste(Jahr,"-",cal$Monat,"-",cal$TagZahl,sep=""),"%Y-%b-%e")))
+  cal$TimeAppoint$year[cal$TimeAppoint > as.POSIXlt(as.Date("2020-03-01"))] <- cal$TimeAppoint$year[cal$TimeAppoint > as.POSIXlt(as.Date("2020-03-01"))]-1
+  cal$TimeSeen <- as.numeric(cal$TimeAppoint)
+  cal$TimeDeleted <- as.numeric("Test")
+  write.csv2(cal, "Kalender.csv", row.names = F, quote = F)
+    
+  return(cal)
+  
 }
