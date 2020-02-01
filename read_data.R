2a96f17fa8951a41b0f30f6bdc030e5646077ffd
Sebastian Scholz <bastiman1@gmail.com>
2020-02-01 14:36:45 +0100

    Abrufen der CSV von pastebin

--- read_data.R
+++ read_data.R
@@ -1,8 +1,36 @@
+library(RCurl)
+library(jsonlite)
+library(XML)
 root_path <- ""
 Sys.setlocale("LC_TIME", "en_US.UTF-8")
 
 get_calendar_csv <- function(){
   cal <- read.csv(paste(root_path,"Kalender.csv",sep=""),sep = ";")
+  cal$TimeFirstSeen <- as.POSIXct(cal$TimeFirstSeen,origin = "1970-01-01",tz = "CET")
+  cal$TimeLastSeen <- as.POSIXct(cal$TimeLastSeen,origin = "1970-01-01",tz = "CET")
+  
+  return(cal)
+}
+
+get_pastebin_csv <- function(){
+  pastebin_pastes <- postForm(
+    uri = 'http://pastebin.com/api/api_post.php',
+    api_user_name = "scholzsebastian",
+    api_dev_key = "012922d95854551f49006bd3efd75401",
+    api_user_key = "75bfb663bdb1eea2f70bcb8b782da84e",
+    api_option = "list")
+  #root node hinzufügen
+  pastebin_pastes <- paste("<div>",pastebin_pastes,"</div>",sep="")
+  pastebin_pastes <- xmlToDataFrame(pastebin_pastes)
+  
+  paste_key <- as.character(pastebin_pastes$paste_key[pastebin_pastes$paste_title=="OpenEuCalendar"])
+  
+  eu_paste <- postForm(uri = 'http://pastebin.com/api/api_raw.php',
+                       api_dev_key = "012922d95854551f49006bd3efd75401",
+                       api_user_key = "75bfb663bdb1eea2f70bcb8b782da84e",
+                       api_paste_key = paste_key,
+                       api_option = "show_paste")
+  cal <- read.csv(text = eu_paste,sep=";")
   cal$TimeFirstSeen <- as.POSIXct(cal$TimeFirstSeen,origin = "1970-01-01",tz = "CET")
   cal$TimeLastSeen <- as.POSIXct(cal$TimeLastSeen,origin = "1970-01-01",tz = "CET")
   
